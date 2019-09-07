8f2f6406702810c020bb1229f20c58639bb2f435
Shawn <43576702+saclark12000@users.noreply.github.com>
2019-09-07 12:09:48 -0500

    updated search function of adoptable pets

--- routes/search.js
+++ routes/search.js
@@ -20,24 +20,52 @@ router.get("/", function(req, res){
 
 //Show route, returns results for all adoptable pets and text search done by user
 router.post("/results", function(req, res){
-    Pet.find({
-        adoptable: {
-            $eq: true
-        },
-        $text:
-        {
-        $search: req.body.search,
-        $language: "en",
-        $caseSensitive: false,
-        $diacriticSensitive: false
-        }
-        }, function(err, searchResults){
-        if(err){
-            console.log(err)
-        } else {
-            res.render("search/index", {results: searchResults, term: req.body.search});
-        }
-    });
+
+    if (req.body.search.toLowerCase() == "dog" || req.body.search.toLowerCase() == "cat"){
+        Pet.find({
+            adoptable: {
+                $eq: true
+            },
+            species : {$eq: req.body.search.toLowerCase()}
+            }, function(err, searchResults){
+            if(err){
+                console.log(err)
+            } else {
+                res.render("search/index", {results: searchResults, term: req.body.search});
+            }
+        });
+    } else {
+        Pet.find({
+            adoptable: {
+                $eq: true
+            },
+            $or : [{
+                species: {
+                    $regex: req.body.search.split(" ").map(str => "("+str+")").join('|'),
+                    $options: "i"
+                }},{
+                breed: {
+                    $regex: req.body.search.split(" ").map(str => "("+str+")").join('|'),
+                    $options: "i"
+                }},{
+                description: {
+                    $regex: req.body.search.split(" ").map(str => "("+str+")").join('|'),
+                    $options: "i"
+                }},{
+                name: {
+                    $regex: req.body.search.split(" ").map(str => "("+str+")").join('|'),
+                    $options: "i"
+                }}
+
+            ]
+            }, function(err, searchResults){
+            if(err){
+                console.log(err)
+            } else {
+                res.render("search/index", {results: searchResults, term: req.body.search});
+            }
+        });
+    }
 });
 
 module.exports = router;
